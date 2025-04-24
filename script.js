1cd06b9676e6df3adf170b95dd9b7a16e167a1f4
Cherinet Demeke Lemma <163031744+Cherireal7@users.noreply.github.com>
2025-04-24 14:29:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,15 @@
         observer.observe(element);
     });
 
+
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/680a1ef77161c519104d9ad5/1ipjohug6';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
+
